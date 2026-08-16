1081ca90088e7cf9548b25fad13d32526ab214b1
Ian Brown <ianbrown500@gmail.com>
2026-08-16 11:09:55 -0400

    create basic structure

---
+++ include/fft.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <complex>
+#include <vector>
+
+std::vector<std::complex<double>> dft(const std::vector<std::complex<double>>& input);
+
+std::vector<std::complex<double>> fft(const std::vector<std::complex<double>>& input);
