6bddf4695cc49103c705bd8313cda919e1d89011
Ian Brown <ianbrown500@gmail.com>
2026-08-16 15:09:25 -0400

    add README and sinusoid test case

---
+++ README.md
@@ -0,0 +1,29 @@
+# from-scratch-fft
+
+## About
+
+Trying to write my own FFT algorithm from scratch without help.
+
+## How to Run
+
+### Build
+
+From the project root:
+
+```bash
+mkdir -p build
+cd build
+cmake ..
+cmake --build .
+```
+
+### Run Main
+
+```bash
+./from-scratch-fft
+```
+
+### Run Tests
+```bash
+./test_fft
+```
--- tests/test_fft.cpp
+++ tests/test_fft.cpp
@@ -26,6 +26,7 @@ void test(const std::string& name, const std::vector<std::complex<double>>& inpu
 
 int main() {
     
+    // Define test vectors
     const int test_size = 64;
     std::vector<std::complex<double>> input(test_size);
     std::vector<std::complex<double>> expected(test_size);
@@ -37,14 +38,24 @@ int main() {
     }
     test(std::string("Impulse test"), input, expected);
 
-    // Constant test
-    std::fill(input.begin(), input.end(), 0);
+    // DC test
+    std::fill(input.begin(), input.end(), 0); // clear vectors
     std::fill(expected.begin(), expected.end(), 0);
     for (int i = 0; i < test_size; i++) {
         input[i] = std::complex<double>(1.0, 2.0);
     }  
     expected[0] = std::complex<double>(1.0 * test_size, 2.0 * test_size);
-    test(std::string("Constant test"), input, expected);
+    test(std::string("DC test"), input, expected);
+
+    // Sinusoid test
+    const int bin = test_size / 2 - 1; // slightly arbitrary
+    std::fill(input.begin(), input.end(), 0);
+    std::fill(expected.begin(), expected.end(), 0);
+    for (int i = 0; i < test_size; i++) {
+        input[i] = std::exp(std::complex<double>(0.0, 2 * M_PI * i * bin / test_size));
+    }
+    expected[bin] = std::complex<double>(test_size, 0.0);
+    test(std::string("Sinusoid test"), input, expected);
 
     return 0;
 }
